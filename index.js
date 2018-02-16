8407dbaa53c64f92bc230c9e2afd96708b0e1bff
Andrew Vos <andrew.vos@gmail.com>
2018-02-16 15:35:16 +0000

    Configure saml stuff with env vars

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 const {create} = require('fake-sso-idp')
 const app = create({
   serviceProvider: {
-    metadata: 'http://indigo-slp.herokuapp.com/clients/testsamlclient/auth/saml/metadata',
-    destination: 'http://indigo-slp.herokuapp.com/clients/testsamlclient/auth/saml/acs'
+    metadata: process.env.SAML_METADATA,
+    destination: process.env.SAML_DESTINATION
   },
-      users: [
-        {
-          id: 'test1',
-          name: 'susan@email.com',
-          username: 'test1',
-          password: 'pwd',
-          attributes: {
-            emailAddress: {
-              format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-              value: 'susan@email.com',
-              type: 'xs:string'
-            },
-            firstName: {
-              format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-              value: 'Sue',
-              type: 'xs:string'
-            },
-            lastName: {
-              format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-              value: 'Sueson',
-              type: 'xs:string'
-            },
-            employeeIdentifier: {
-              format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-              value: 'employee-666',
-              type: 'xs:string'
-            }
-          }
+  users: [
+    {
+      id: 'test1',
+      name: 'susan@email.com',
+      username: 'test1',
+      password: 'pwd',
+      attributes: {
+        emailAddress: {
+          format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+          value: 'susan@email.com',
+          type: 'xs:string'
         },
-        {
-          id: 'fubar',
-          name: 'Test user 2',
-          username: 'fubar',
-          password: 'pwd',
-          attributes: {
-            pisa_id: {
-              format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
-              value: 'fubar',
-              type: 'xs:string'
-            }
-          }
+        firstName: {
+          format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+          value: 'Sue',
+          type: 'xs:string'
+        },
+        lastName: {
+          format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+          value: 'Sueson',
+          type: 'xs:string'
+        },
+        employeeIdentifier: {
+          format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+          value: 'employee-666',
+          type: 'xs:string'
         }
-      ]
+      }
+    },
+    {
+      id: 'fubar',
+      name: 'Test user 2',
+      username: 'fubar',
+      password: 'pwd',
+      attributes: {
+        pisa_id: {
+          format: 'urn:oasis:names:tc:SAML:2.0:attrname-format:uri',
+          value: 'fubar',
+          type: 'xs:string'
+        }
+      }
+    }
+  ]
 })
 
 const port = Number(process.env.PORT || 7000)
-app.options.audience = 'http://indigo-slp.herokuapp.com//clients/testsamlclient/auth/saml/metadata'
+app.options.audience = process.env.SAML_AUDIENCE
 console.log(`Started on port ${port}...`)
 app.listen(port)
